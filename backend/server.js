007360d33851b5d567426285713bab5bed924447
DhrubaDC1 <dhruba.chakraborty.2000@gmail.com>
2023-05-23 18:16:29 +0600

    First update

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express")
+const mongoose = require("mongoose")
+const workoutRoutes = require("./routes/workouts");
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+//routes
+app.use("/api/workouts", workoutRoutes)
+
+// connect to db
+mongoose.connect("mongodb+srv://dhruba:android98@mernapp.xog5si4.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
